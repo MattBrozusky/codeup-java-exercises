452a4caa884544b69ae6ace0cf8cc52e9ea352f6
MattBrozusky <matthew.brozusky@gmail.com>
2018-11-08 14:10:22 -0600

    Starting string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
