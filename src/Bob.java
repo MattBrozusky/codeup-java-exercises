f285f02c9858c7abf97ea437908c24ea6ec8948b
MattBrozusky <matthew.brozusky@gmail.com>
2018-11-09 10:51:39 -0600

    started method exercise

--- src/Bob.java
+++ src/Bob.java
@@ -13,37 +13,37 @@ public class Bob {
 //        Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
 
 
-//        System.out.println("Say something you want to Bob. Make sure you use proper punctuation");
-//
-//        String response, keepGoing;
-//
-//       finish:
-//       do {
-//           response = sc.nextLine();
-//           if (response.endsWith("?")){
-//               System.out.println("Sure.");
-//           } else if (response.endsWith("!")){
-//               System.out.println("Whoa, chill out!");
-//           } else if (response.equalsIgnoreCase("")){
-//               System.out.println("Fine. Be that way!");
-//           } else {
-//               System.out.println("Whatever.");
-//           }
-//
-//           do {
-//               System.out.println("Would you like to say something else to Bob?");
-//               keepGoing = sc.nextLine();
-//               if (keepGoing.equalsIgnoreCase("yes")){
-//                   System.out.println("Out with it then.");
-//                   break;
-//               } else if (keepGoing.equalsIgnoreCase("no")){
-//                   System.out.println("Cya");
-//                   break finish;
-//               }
-//               System.out.println("Bob is simple, just say yes or no.");
-//           } while (true);
-//
-//       } while (keepGoing.equalsIgnoreCase("yes"));
+        System.out.println("Say something you want to Bob. Make sure you use proper punctuation");
+
+        String response, keepGoing;
+
+       finish:
+       do {
+           response = sc.nextLine();
+           if (response.endsWith("?")){
+               System.out.println("Sure.");
+           } else if (response.endsWith("!")){
+               System.out.println("Whoa, chill out!");
+           } else if (response.equalsIgnoreCase("")){
+               System.out.println("Fine. Be that way!");
+           } else {
+               System.out.println("Whatever.");
+           }
+
+           do {
+               System.out.println("Would you like to say something else to Bob?");
+               keepGoing = sc.nextLine();
+               if (keepGoing.equalsIgnoreCase("yes")){
+                   System.out.println("Out with it then.");
+                   break;
+               } else if (keepGoing.equalsIgnoreCase("no")){
+                   System.out.println("Cya");
+                   break finish;
+               }
+               System.out.println("Bob is simple, just say yes or no.");
+           } while (true);
+
+       } while (keepGoing.equalsIgnoreCase("yes"));
 
 
 
@@ -58,37 +58,102 @@ public class Bob {
 //        output - 5 vowels and 10 consonants
 
 
-        System.out.println("Enter any sentence you want.");
-        String response = sc.nextLine();
-        int vowels = 0, consonants = 0;
+//        System.out.println("Enter any sentence you want.");
+//        String response = sc.nextLine();
+//        int vowels = 0, consonants = 0;
+//
+//        for (int i = 0; i < response.length(); i++){
+//            char thisChar = response.charAt(i);
+//            thisChar = Character.toUpperCase(thisChar);
+//            switch (thisChar) {
+//                case 'A':
+//                    vowels += 1;
+//                    break;
+//                case 'E':
+//                    vowels += 1;
+//                    break;
+//                case 'I':
+//                    vowels += 1;
+//                    break;
+//                case 'O':
+//                    vowels += 1;
+//                    break;
+//                case 'U':
+//                    vowels += 1;
+//                    break;
+//                case ',': case '.': case '!': case '?': case '\'': case '"':
+//                    break;
+//                default:
+//                    consonants += 1;
+//                    break;
+//            }
+//        }
+//        System.out.format("Here is the number of vowels: %d\nAnd here is the number of consonants: %d", vowels, consonants);
 
-        for (int i = 0; i < response.length(); i++){
-            char thisChar = response.charAt(i);
-            thisChar = Character.toUpperCase(thisChar);
-            switch (thisChar) {
-                case 'A':
-                    vowels += 1;
-                    break;
-                case 'E':
-                    vowels += 1;
-                    break;
-                case 'I':
-                    vowels += 1;
-                    break;
-                case 'O':
-                    vowels += 1;
-                    break;
-                case 'U':
-                    vowels += 1;
-                    break;
-                case ',': case '.': case '!': case '?': case '\'': case '"':
-                    break;
-                default:
-                    consonants += 1;
-                    break;
-            }
-        }
-        System.out.format("Here is the number of vowels: %d\nAnd here is the number of consonants: %d", vowels, consonants);
+
+
+
+//        Create date format converter application.
+//                Take in the following format:
+//
+//        MM/DD/YYYY
+//
+//        Output the following:
+//
+//        MonthName DD, YYYY
+//
+//        Example:
+//
+//        input - 12/01/1999
+//        output - December 12, 1999
+
+
+//        System.out.println("Enter a date in the MM/DD/YYYY format.");
+//        String date = sc.nextLine();
+//
+//
+//        String monthNum = date.substring(0, 2), monthSpelled = "", dayNum = date.substring(3, 5), yearNum = date.substring(6, 10);
+//
+//        switch (monthNum) {
+//                case "01":
+//                    monthSpelled = "January";
+//                    break;
+//                case "02":
+//                    monthSpelled = "February";
+//                    break;
+//                case "03":
+//                    monthSpelled = "March";
+//                    break;
+//                case "04":
+//                    monthSpelled = "April";
+//                    break;
+//                case "05":
+//                    monthSpelled = "May";
+//                    break;
+//                case "06":
+//                    monthSpelled = "June";
+//                    break;
+//                case "07":
+//                    monthSpelled = "July";
+//                    break;
+//                case "08":
+//                    monthSpelled = "August";
+//                    break;
+//                case "09":
+//                    monthSpelled = "September";
+//                    break;
+//                case "10":
+//                    monthSpelled = "October";
+//                    break;
+//                case "11":
+//                    monthSpelled = "November";
+//                    break;
+//                case "12":
+//                    monthSpelled = "December";
+//                    break;
+//            }
+//
+//        System.out.format("%s %s, %s", monthSpelled, dayNum, yearNum);
 
 
 
---
+++ src/MethodsExercises.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+//        System.out.println(addition(1, 5));
+//        System.out.println(subtraction(1, 5));
+//        System.out.println(multiplication(1, 5, 0));
+//        System.out.println(division(5, 5));
+//        System.out.println(modulus(10, 5));
+
+//        System.out.print("Enter a number between 1 and 10: ");
+//        int userInput = getInteger(1, 10);
+
+    }
+
+
+//    public static int addition(int num1, int num2){
+//        return num1 + num2;
+//    }
+//
+//    public static int subtraction(int num1, int num2){
+//        return num1 - num2;
+//    }
+//
+//    public static int multiplication(int num1, int num2, int total){
+////        for (int i = num2; i > 0; i -= 1){
+////            total += num1;
+////        }
+////        return total;
+//        if (num2 == 0){
+//            return total;
+//        } else {
+//            total += num1;
+//            return multiplication(num1, num2 - 1, total);
+//        }
+//    }
+//
+//    public static int division(int num1, int num2){
+//        return num1 / num2;
+//    }
+//
+//    public static int modulus(int num1, int num2){
+//        return num1 % num2;
+//    }
+
+
+//
+//    public static int getInteger(int min, int max){
+//        Scanner sc = new Scanner(System.in);
+//        int input = Integer.parseInt(sc.nextLine());
+//        if (input <= 0 || input >= 11){
+//            System.out.println("It needs to be between 1 and 10.");
+//            return getInteger(1, 10);
+//        } else {
+//            System.out.format("%d is a correct input. Good job!", input);
+//            return 0;
+//        }
+//    }
+
+
+//    Calculate the factorial of a number.
+//
+//    Prompt the user to enter an integer from 1 to 10.
+//    Display the factorial of the number entered by the user.
+//    Ask if the user wants to continue.
+//    Use a for loop to calculate the factorial.
+//    Assume that the user will enter an integer, but verify it’s between 1 and 10.
+//    Use the long type to store the factorial.
+//    Continue only if the user agrees to.
+//    A factorial is a number multiplied by each of the numbers before it.
+//    Factorials are denoted by the exclamation point (n!). Ex:
+//
+//
+//            1! = 1               = 1
+//            2! = 1 x 2           = 2
+//            3! = 1 x 2 x 3       = 6
+//            4! = 1 x 2 x 3 x 4   = 24
+//    Bonus
+//
+//    Test the application and find the integer for the highest factorial that can be accurately calculated by this application, then modify the prompt so that it prompts the user for a number "from 1 to {the highest integer that returns accurate factorial calculation}". Don’t forget to change your verification too!
+//    Use Recursion to implement the factorial.
+
+
+
+
+
+}
