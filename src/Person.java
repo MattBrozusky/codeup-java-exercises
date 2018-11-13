1b8798c0d72fd908ab2c4416c3defc22067dad74
MattBrozusky <matthew.brozusky@gmail.com>
2018-11-13 09:22:32 -0600

    finished Person part of exercise

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,50 @@
 public class Person {
+    private String name;
+
+    // returns the person's name
+    public String getName(){
+        return this.name;
+    }
+
+    // changes the name property to the passed value
+    public void setName(String name){
+        this.name = name;
+    }
+
+    // prints a message to the console using the person's name
+    public void sayHello(){
+        System.out.format("Hello, %s", getName());
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Matt");
+        p1.sayHello();
+        p1.setName("Bob");
+        p1.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
+
+    }
+
+
 }
