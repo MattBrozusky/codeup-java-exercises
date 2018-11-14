bff6884abd739c3a59cf9afbbabd0a1e1bda5808
MattBrozusky <matthew.brozusky@gmail.com>
2018-11-14 11:35:14 -0600

    finished ServerNameGenerator

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,23 @@
+import java.util.Random;
 public class ServerNameGenerator {
+
+    public static String[] adjectives = {"quickly", "ambitious", "aggressive", "agreeable", "brave", "calm", "delightful", "eager", "faithful", "fat"};
+
+    public static String[] nouns = {"dog", "cat", "tree", "cup", "person", "plant", "building", "desk", "car", "apple"};
+
+
+    public static String randomElement(String[] array){
+        return array[new Random().nextInt(array.length)];
+    }
+
+
+    public static void main(String[] args) {
+
+        String randomAdjective = randomElement(adjectives), randomNoun = randomElement(nouns);
+
+        System.out.format("Here is your random adjective-noun: %s-%s", randomAdjective, randomNoun);
+
+    }
+
+
 }
